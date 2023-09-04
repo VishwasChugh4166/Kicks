71efad4198d4f585ebe6f17e80b27a723d545faa
VishwasChugh4166 <126945758+VishwasChugh4166@users.noreply.github.com>
2023-09-04 13:38:56 +0530

    Third commit

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url); //get current directory of the script
 
 //rest object
 const app = express();
@@ -22,7 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__filename, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -31,7 +34,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html  "));
+  res.sendFile(path.join(__filename, "./client/build/index.html  "));
 });
 
 //PORT
